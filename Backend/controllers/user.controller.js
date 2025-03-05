887d874eadfc16fe4393e24e578827716659ef2a
Chaitanya Bhavar <chaitanyabhavar0@gmail.com>
2025-03-05 17:04:16 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,9 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-const BlackListToken = require("../models/blacklistToken.model");
+//const BlackListToken = require("../models/blacklistToken.model");
+  const blackListTokenModel = require('../models/blacklistToken.model');
+
 // ✅ Correct import
 
 module.exports.registerUser = async (req, res, next) => {
